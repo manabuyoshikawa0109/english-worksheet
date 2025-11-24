ce0fdddb5afdb0fa58aef3dbc212442ea396c328
吉川学 <>
2025-11-24 23:31:56 +0900

    ベースとなるHTML、CSS、フォントファイルを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,137 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+export default function EnglishWorksheet() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    // JSXでは1つの要素を返す必要がある為、全体を1つの要素で囲う必要がある
+    // しかしdivタグ等で囲うと無駄にネストすることになる為react fragmentを使う
+    <>
+      <div id="sidebar" className="sidebar">
+
+        <div className="sidebar-inner">
+          <h4>ワークシートのタイトル</h4>
+          <input id="worksheetTitleInput" type="text" className="form-control" placeholder="例）Today's weather" />
+
+          <hr />
+
+          <h4 className="mt-4">文字のスタイル</h4>
+          <div className="d-flex align-items-center gap-2">
+            <div className="input-group w-auto">
+              <input type="color" id="textColor" className="form-control form-control-color" />
+              <span className="input-group-text">色</span>
+            </div>
+            <div className="btn-group" role="group" aria-label="文字のスタイル">
+              <button id="btnBold" className="btn btn-primary fw-bold" title="太字">
+                B
+              </button>
+              <button id="btnItalic" className="btn btn-primary fst-italic" title="イタリック">
+                I
+              </button>
+              <button id="btnUnderline" className="btn btn-primary text-decoration-underline" title="下線">
+                U
+              </button>
+            </div>
+          </div>
+
+          <hr />
+
+          <h4 className="mt-4">罫線のスタイル</h4>
+          <div className="input-group mb-4">
+            <span className="input-group-text">第</span>
+            <select id="targetLine" className="form-select">
+              <option value="line1">1</option>
+              <option value="line2">2</option>
+              <option value="line3">3</option>
+              <option value="line4">4</option>
+            </select>
+            <span className="input-group-text">罫線</span>
+          </div>
+          <div className="input-group">
+            <select id="lineStyle" className="form-select">
+              <option value="solid">直</option>
+              <option value="dotted">点</option>
+              <option value="dashed">ダッシュ</option>
+            </select>
+            <span className="input-group-text">線</span>
+            <input id="lineColor" type="color" className="form-control form-control-color" />
+            <span className="input-group-text">色</span>
+          </div>
+
+          <hr />
+
+          <h4 className="mt-4">行の幅</h4>
+          <div className="input-group mb-4">
+            <select id="targetLineInput" className="form-select"></select>
+            <span className="input-group-text">行目</span>
+            <input id="lineInputScaleValue" type="number" className="form-control" value="100" min="0" max="100" step="1" />
+            <span className="input-group-text">%</span>
+          </div>
+          <input id="lineInputScaleRange" type="range" className="w-100" value="100" min="0" max="100" step="1" />
+
+          <hr />
+
+          <h4 className="mt-4">その他の操作</h4>
+          <div className="d-flex align-items-center gap-2">
+            <button id="downloadPdfBtn" className="btn btn-primary w-50">
+              <i className="fa-regular fa-file-pdf"></i>
+              PDF出力
+            </button>
+            <button id="clearBtn" className="btn btn-danger w-50">
+              <i className="fa-solid fa-trash-can"></i>
+              クリア
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+
+      {/* ワークシート */}
+      <div className="main-content">
+        <nav className="header navbar">
+          <div className="container-fluid">
+            <a id="toggleSidebar" className="navbar-brand text-white">
+              <i className="fa-solid fa-bars"></i>
+            </a>
+          </div>
+        </nav>
+        <div className="worksheet-container">
+          <div id="worksheet" className="worksheet">
+            <div className="worksheet-header">
+              <div className="student-info">
+                <div className="student-info-box">
+                  <div className="student-info-label">Grade</div>
+                  {/*
+                    input要素だとhtml2canvasでキャプチャした際に入力文字が上に配置され、
+                    切れてしまう不具合があるためcontentEditableを使用
+                    参考：https://github.com/niklasvh/html2canvas/issues/2008
+                  */}
+                  <div className="student-info-input" contentEditable="true"></div>
+                </div>
+                <div className="student-info-box">
+                  <div className="student-info-label">Class</div>
+                  <div className="student-info-input" contentEditable="true"></div>
+                </div>
+                <div className="student-info-box">
+                  <div className="student-info-label">No.</div>
+                  <div className="student-info-input" contentEditable="true"></div>
+                </div>
+              </div>
+
+              <div className="line-group student-name">
+                <div className="line line1"></div>
+                <div className="line line2"></div>
+                <div className="line line3"></div>
+                <div className="line line4"></div>
+                <div className="student-name-label">
+                  <span>Name</span><span>Date<span className="date"> ・・</span></span>
+                </div>
+                <div className="line-input" contentEditable="true"></div>
+              </div>
+            </div>
+
+            <div id="worksheetTitle" className="worksheet-title"></div>
+            <div id="line-groups"></div>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
